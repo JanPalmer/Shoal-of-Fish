60078a54722e4bf8d317d2f7f21c5da3baa0e54c
Krzysztof Czerwonopolski <83087348+JanPalmer@users.noreply.github.com>
2023-01-18 10:27:30 +0100

    Delete Shoal_of_Fish directory

--- Shoal_of_Fish/boids_movementGPU.cuh
+++
@@ -1,8 +0,0 @@
-#include "utilities.cuh"
-#include "boids.h"
-
-__host__ __device__ bool isInSight(s_boids* boids, int index, int other);
-__host__ __device__ void centerOfMassRule(s_boids* boids, int index, float deltaTime);
-__host__ __device__ void separationRule(s_boids* boids, int index, float deltaTime);
-__host__ __device__ void alignmentRule(s_boids* boids, int index, float deltaTime);
-__global__ void moveBoidsGPU(s_boids* boids, float deltaTime);
